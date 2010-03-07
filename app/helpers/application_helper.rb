0e5ebe5286bf4d3f9a9af0b964f50808cee17da2
Alexandre "kAworu" Perrin <kaworu(a)kaworu,ch>
2010-03-07 16:58:37 +0100

    Added BlueCloth and CodeRay support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,28 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # our own markdown function that support coderay
+  def markdown text
+    result  = String.new
+    code    = nil
+    lang    = nil
+
+    text.split("\n").each do |line|
+      if lang
+        if line =~ /^\s*%end\s*code\s*$/
+          result << CodeRay.scan(code, lang).div
+          lang = nil
+        else
+          code << line << "\n"
+        end
+      else
+        if line =~ /^\s*%code\s+(\w+)\s*$/
+          lang = $1
+          code = String.new
+        else
+          result << line << "\n"
+        end
+      end
+    end
+    BlueCloth.new(result).to_html
+  end
 end

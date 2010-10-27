25cfaf5e8b1eadcfe00b19072dd40316d61e651c
Alexandre "kAworu" Perrin <kaworu@kaworu.ch>
2010-10-27 19:16:37 +0200

    use rails v2.3.8

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -21,12 +21,12 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   #
 #  config.gem 'friendly_id', :version => '2.3.1'
-  config.gem 'haml',        :version => '2.2.20'
-  config.gem 'BlueCloth',   :version => '1.0.1',   :lib => 'bluecloth'
-  config.gem 'coderay',     :version => '0.9.1'
-  config.gem 'builder',     :version => '2.1.2'
-  config.gem 'friendly_id', :version => '>= 2.3.0'
-  config.gem 'authlogic',   :version => '2.1.3'
+  config.gem 'haml'
+  config.gem 'BlueCloth', :lib => 'bluecloth'
+  config.gem 'coderay'
+  config.gem 'builder'
+  config.gem 'friendly_id'
+  config.gem 'authlogic'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

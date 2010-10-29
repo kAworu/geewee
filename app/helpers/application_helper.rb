ad443c70a65e1c56fae8ed37cb985423a84191f8
Alexandre "kAworu" Perrin <kaworu@kaworu.ch>
2010-10-30 00:34:24 +0200

    number to words :)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,7 +44,7 @@ module ApplicationHelper
     BlueCloth.new(result).to_html
   end
 
-  # disable html support in mkd
+  # disable html support in mkd, used to render comments.
   def markdown_no_html text
     BlueCloth.new(text, :filter_html).to_html
   end
@@ -69,5 +69,85 @@ module ApplicationHelper
   # time_ago_in_word.
   def time_ago_sentance time
     t('time_ago', :time => time_ago_in_words(time))
+  end
+
+
+  # helper to display the comment count. Choose the right key to translate.
+  def comments_count count
+    case count
+    when 0 then t('meta.no_comments')
+    when 1 then t('meta.one_comment')
+    else        "#{number_to_word(count)} #{t 'meta.comments'}"
+    end
+  end
+
+  # translate a number into word. see http://pine.fm/LearnToProgram/?Chapter=08.
+  # we only need to handle number > 1.
+  #
+  # XXX: this implementation sucks when number >= 1000 (as explainted in the
+  #      link). It's our own version of the "year 2000 bug". If it happens
+  #      sometimes, fixing it will be easy though ;)
+  def number_to_word number
+    numString = ''  #  This is the string we will return.
+
+    #  "left" is how much of the number we still have left to write out.
+    #  "write" is the part we are writing out right now.
+    #  write and left... get it?  :)
+    left  = number
+    write = left / 100          #  How many hundreds left to write out?
+    left  = left - write * 100  #  Subtract off those hundreds.
+
+    if write > 0
+      #  Now here's a really sly trick:
+      hundreds   = number_to_word(write)
+      numString << hundreds + ' ' + t('numbers.100')
+      #  That's called "recursion".  So what did I just do?
+      #  I told this method to call itself, but with "write" instead of
+      #  "number".  Remember that "write" is (at the moment) the number of
+      #  hundreds we have to write out.  After we add "hundreds" to "numString",
+      #  we add the string ' hundred' after it.  So, for example, if
+      #  we originally called englishNumber with 1999 (so "number" = 1999),
+      #  then at this point "write" would be 19, and "left" would be 99.
+      #  The laziest thing to do at this point is to have englishNumber
+      #  write out the 'nineteen' for us, then we write out ' hundred',
+      #  and then the rest of englishNumber writes out 'ninety-nine'.
+
+      if left > 0
+        #  So we don't write 'two hundredfifty-one'...
+        numString << ' '
+      end
+    end
+
+    write = left / 10          #  How many tens left to write out?
+    left  = left - write * 10  #  Subtract off those tens.
+
+    if write > 0
+      if write == 1 and left > 0
+        #  Since we can't write "tenty-two" instead of "twelve",
+        #  we have to make a special exception for these.
+        numString << t("numbers.#{10 + left}")
+
+        #  Since we took care of the digit in the ones place already,
+        #  we have nothing left to write.
+        left = 0
+      else
+        numString << t("numbers.#{write * 10}")
+      end
+
+      if left > 0
+        #  So we don't write 'sixtyfour'...
+        numString << '-'
+      end
+    end
+
+    write = left  #  How many ones left to write out?
+    left  = 0     #  Subtract off those ones.
+
+    if write > 0
+      numString << t("numbers.#{write}")
+    end
+
+    #  Now we just return "numString"...
+    numString
   end
 end

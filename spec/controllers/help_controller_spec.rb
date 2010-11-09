20fa419bf96ecda6e392534d3ffdbb74b28e5077
Alexandre "kAworu" Perrin <kaworu@kaworu.ch>
2010-11-09 01:37:58 +0100

    Added spec for installation help page.

--- spec/controllers/help_controller_spec.rb
+++ spec/controllers/help_controller_spec.rb
@@ -2,18 +2,12 @@ require 'spec_helper'
 
 describe HelpController do
   context 'when not configured' do
-    %w[index unauthorized config api man development].each do |action|
+    %w[index unauthorized config api man development installation].each do |action|
       it "#{action} should not redirect to avoid infinit redirect" do
         get action.to_sym
         response.should_not redirect_to config_path
         response.should be_success
       end
-    end
-  end
-
-  context 'when configured' do
-    before :each do
-      Factory.create :geewee_config
     end
   end
 end
